98ddc921fc2c540f84969472b50817de7cc0ff7b
davidemarcoli <davide@marcoli.ch>
2023-09-15 22:35:43 +0200

    Fix weird import https://github.com/nextauthjs/next-auth/issues/7377

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, {NextAuthOptions} from "next-auth/index";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
